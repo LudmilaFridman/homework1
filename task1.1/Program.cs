9c69b7aadfd355bd1a0667dc82d4cf66fed29bee
fridmanludmila <fridmanludmila@yandex.ru>
2022-11-03 16:57:01 +0400

    added task 1.1

---
+++ task1.1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("enter а=");
+int a = int.Parse(Console.ReadLine());
+Console.Write("enter b=");
+int b = int.Parse(Console.ReadLine());
+
+int max = 0;
+int min = 0;
+
+if(a > b)
+{   max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}
+Console.Write("max=");
+Console.WriteLine(max);
+Console.Write("min=");
+Console.WriteLine(min);
